6b1d500074d95c33c56a8d4400e8d0d538e8c2b0
Fabio Filho <fabiofilho892@gmail.com>
2023-07-15 18:56:56 -0300

    script completo versao 1

--- script.js
+++ script.js
@@ -2,8 +2,24 @@
     return Math.floor(Math.random() * max)
 }
 
-const userInt = prompt("Rock, Paper & Scissors! Escolha 0 para Papel, 1 para Pedra, ou 2 para Tesoura.")
+const userInt = parseInt(prompt("Rock, Paper & Scissors! Escolha 0 para Papel, 1 para Pedra, ou 2 para Tesoura."))
+console.log(`Você escolheu ${userInt}.`)
 
 function play() {
-    
+    const enemy = randomGen(3)
+    if (userInt == 0 && enemy == 1 || userInt == 1 && enemy == 2 || userInt == 2 && enemy == 0) {
+        console.log(`O inimigo escolheu ${enemy}.`)
+        console.log("Ah, você perdeu.")
+
+    } else if (userInt == 0 && enemy == 2 || userInt == 1 && enemy == 0 || userInt == 2 && enemy == 1) {
+        console.log(`O inimigo escolheu ${enemy}.`)
+        console.log("Ah, você venceu!")
+
+    } else {
+        console.log(`O inimigo escolheu ${enemy}.`)
+        console.log("Eita, deu empate!")
+    }
+
 }
+
+play()
