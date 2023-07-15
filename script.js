d0c4ab239e155b762bc03a5da3a3550e7179b591
Fabio Filho <fabiofilho892@gmail.com>
2023-07-15 14:43:32 -0300

    "function que cria um random numb de 0 a 2"

---
+++ script.js
@@ -0,0 +1,9 @@
+ function randomGen (max) {
+    return Math.floor(Math.random() * max)
+}
+
+const userInt = prompt("Rock, Paper & Scissors! Escolha 0 para Papel, 1 para Pedra, ou 2 para Tesoura.")
+
+function play() {
+    
+}
