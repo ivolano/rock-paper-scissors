1ce0e93408fc75b88888dd4209c228ca1ad32fd4
Fabio Filho <fabiofilho892@gmail.com>
2023-07-15 20:17:19 -0300

    js optimization

--- script.js
+++ script.js
@@ -7,19 +7,20 @@ console.log(`Você escolheu ${userInt}.`)
 
 function play() {
     const enemy = randomGen(3)
+    console.log(`O inimigo escolheu ${enemy}.`)
     if (userInt == 0 && enemy == 1 || userInt == 1 && enemy == 2 || userInt == 2 && enemy == 0) {
-        console.log(`O inimigo escolheu ${enemy}.`)
+       
         console.log("Ah, você perdeu.")
 
     } else if (userInt == 0 && enemy == 2 || userInt == 1 && enemy == 0 || userInt == 2 && enemy == 1) {
-        console.log(`O inimigo escolheu ${enemy}.`)
+       
         console.log("Ah, você venceu!")
 
     } else {
-        console.log(`O inimigo escolheu ${enemy}.`)
+        
         console.log("Eita, deu empate!")
     }
-
+ 
 }
 
 play()
