a5de1471a27e55f8762327b923073dd6b7f0487e
Fabio Filho <fabiofilho892@gmail.com>
2023-07-23 13:28:09 -0300

    jogar usando string ao invés de números

--- script.js
+++ script.js
@@ -2,25 +2,32 @@
     return Math.floor(Math.random() * 3)
 }
 
-const userInt = parseInt(prompt("Rock, Paper & Scissors! Escolha 0 para Papel, 1 para Pedra, ou 2 para Tesoura."))
+const userInt = prompt("Rock, Paper & Scissors! Escolha 0 para Papel, 1 para Pedra, ou 2 para Tesoura.").toUpperCase()
 console.log(`Você escolheu ${userInt}.`)
 
 function play() {
     const enemy = randomGen()
-    console.log(`O inimigo escolheu ${enemy}.`)
-    if (userInt == 0 && enemy == 1 || userInt == 1 && enemy == 2 || userInt == 2 && enemy == 0) {
+    if (enemy == 0) {
+        console.log(`O inimigo escolheu PAPEL`)
+    } else if (enemy == 1) {
+        console.log(`O inimigo escolheu PEDRA`)
+    } else {
+        console.log(`O inimigo escolheu TESOURA`)
+    }
+    if (userInt == "TESOURA" && enemy == 1 || userInt == "PAPEL" && enemy == 2 || userInt == "PEDRA" && enemy == 0) {
        
-        console.log("Ah, você perdeu.")
+        console.log("Você perdeu.")
 
-    } else if (userInt == 0 && enemy == 2 || userInt == 1 && enemy == 0 || userInt == 2 && enemy == 1) {
+    } else if (userInt == "PEDRA" && enemy == 2 || userInt == "TESOURA" && enemy == 0 || userInt == "PAPEL" && enemy == 1) {
        
-        console.log("Ah, você venceu!")
+        console.log("Você venceu!")
 
     } else {
         
         console.log("Eita, deu empate!")
     }
- 
+    
+    
 }
 
 play()
